4eb321f2fd0957d6111528a439eff5a66dbf7413
Noah Dumba <noahdumba1@gmail.com>
2020-01-24 21:12:36 +0000

    implemented all vampire family tree functions

--- vampire.js
+++ vampire.js
@@ -6,37 +6,57 @@ class Vampire {
     this.creator = null;
   }
 
-  /** Simple tree methods **/
-
-  // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
-  // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
-
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
-
+    let vamps = 0;
+    let currentVamp = this;
+    while (currentVamp.creator) {
+      currentVamp = currentVamp.creator;
+      vamps++;
+    }
+    return vamps;
   }
-
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    return (this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal);
   }
-
-  /** Stretch **/
-
   // Returns the closest common ancestor of two vampires.
-  // The closest common anscestor should be the more senior vampire if a direct ancestor is used.
-  // For example:
-  // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
-  // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
   closestCommonAncestor(vampire) {
+    if (this === vampire) {
+      return this;
+    } else if (this.creator === null || vampire.creator === this) {
+      return this;
+    } else if (vampire.creator === null || this.creator === vampire) {
+      return vampire;
+    } else {
+      let vamp1 = this;
+      let vamp2 = vampire;
 
+      while (vamp1.creator !== vamp2.creator) {
+        while (vamp1.numberOfVampiresFromOriginal !== vamp2.numberOfVampiresFromOriginal) {
+
+          if (vamp1.numberOfVampiresFromOriginal > vamp2.numberOfVampiresFromOriginal) {
+            vamp1 = vamp1.creator;
+          } else if (vamp1.numberOfVampiresFromOriginal < vamp2.numberOfVampiresFromOriginal) {
+            vamp2 = vamp2.creator;
+          }
+        }
+
+        if (vamp1.creator !== vamp2.creator && vamp1.numberOfVampiresFromOriginal === vamp2.numberOfVampiresFromOriginal) {
+          vamp1 = vamp1.creator;
+          vamp2 = vamp2.creator;
+        }
+      }
+      return vamp1.creator;
+    }
   }
 }
 
